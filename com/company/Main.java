1bd5257421408a1db999c3c2372212319db8542c
mehtabbal <mehtab4801@gmail.com>
2021-10-27 16:30:39 -0700

    Added another print statement

--- com/company/Main.java
+++ com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hi from Pragra");
+        System.out.println("Added another print statement");
     }
 }
